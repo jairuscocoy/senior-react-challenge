3c8871034d777795dcd1d0e48404516748d04f2f
Jairus Cocoy Gumpal <jairuscocoygumpal@Jairuss-MacBook-Pro.local>
2026-03-31 15:11:32 +0800

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import ReactQueryProvider from "@/providers/react-query-provider";
 import type { Metadata } from "next";
 import "./globals.css";
+import { IBM_Plex_Sans } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const ibmPlexSans = IBM_Plex_Sans({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Senior React Challenge",
@@ -13,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", ibmPlexSans.variable)}>
       <body className={`m-4 antialiased`}>
         <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
